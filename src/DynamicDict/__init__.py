fca1cdb037f0916eb8ca219a010723c3039047dd
Shane <gebnar@gmail.com>
2024-07-26 13:35:46 -0500

    Update version again

--- src/DynamicDict/__init__.py
+++ src/DynamicDict/__init__.py
@@ -1,6 +1,6 @@
 """A dictionary wrapper that allows attribute access and mutation using both dot notation and dictionary-style indexing."""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 # For easy import...
 from .DynamicDict import dynamic
