31ebf53daf1280ce7119b4bfd9e3e32fa82507e7
Shane <gebnar@gmail.com>
2024-07-27 10:47:02 -0500

    fix project publishing details

--- src/dynamic_dict/__init__.py
+++ src/dynamic_dict/__init__.py
@@ -1,6 +1,6 @@
 """A dictionary wrapper that allows attribute access and mutation using both dot notation and dictionary-style indexing."""
 
-__version__ = "0.1.7"
+__version__ = "0.2.0"
 
 # For easy import...
 from .dynamic_dict import Dynamic
